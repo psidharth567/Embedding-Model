a9765b70c9214996302d6aee5507832575e8fc4c
Sidharth Pulipaka <149266412+psidharth567@users.noreply.github.com>
2025-09-03 18:24:24 +0530

    Update AllGather.py

--- InfoNCE_Loss/AllGather.py
+++ InfoNCE_Loss/AllGather.py
@@ -15,21 +15,24 @@ class AllGather(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
+        # THIS IS THE INITIAL CODE THAT I FOUND IN THIS REPO: https://github.com/zsnoob/EfficientDDP-4-Contrastive-Train
+
         # Reduce gradients prior to gradient bucket to avoid behavior mismatches with non-distributed training
-        dist.all_reduce(grad_output, op=dist.ReduceOp.SUM)
+        # dist.all_reduce(grad_output, op=dist.ReduceOp.SUM)
 
-        return (
-            grad_output[ctx.local_batch_size * ctx.rank: ctx.local_batch_size * (ctx.rank + 1)],
-            None,
-        )
+        # return (
+        #     grad_output[ctx.local_batch_size * ctx.rank: ctx.local_batch_size * (ctx.rank + 1)],
+        #     None,
+        # )
 
-    # GPT-5-thinking-high suggested to use reduce-scatter instead of all gather. 
-    # Reduce Scatter is much more efficient but I don't yet know if we can replace that with AllReduce here.
+    # GPT-5-thinking-high suggested to use reduce-scatter instead of all gather and its correct.
+    # Reduce Scatter is much more efficient than AllReduce here and ReduceScatter and AllGather are inverse operations of each-other. It's more efficient to use ReduceScatter
+    # https://jax-ml.github.io/scaling-book/sharding/
 
-    # @staticmethod
-    # def backward(ctx, grad_output):
-    #     # local shard after reduction
-    #     local = grad_output.narrow(0, ctx.local_batch_size * ctx.rank, ctx.local_batch_size)
-    #     out = torch.empty_like(local)
-    #     dist.reduce_scatter_tensor(out, grad_output, op=dist.ReduceOp.SUM)
-    #     return out, None
+    @staticmethod
+    def backward(ctx, grad_output):
+        # local shard after reduction
+        local = grad_output.narrow(0, ctx.local_batch_size * ctx.rank, ctx.local_batch_size)
+        out = torch.empty_like(local)
+        dist.reduce_scatter_tensor(out, grad_output, op=dist.ReduceOp.SUM)
+        return out, None
